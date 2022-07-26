eb3b5829e82ba620a40362d35f68f5a0b2867e17
Prasad Pawtekar <prasad@rjtcompuquest.com>
2022-07-27 03:10:43 +0530

    Add AllEmployesAcitivity.

---
+++ app/src/main/java/com/apolisrises/navigationgraphdemo/ui/employee/AllEmployeesActivity.kt
@@ -0,0 +1,12 @@
+package com.apolisrises.navigationgraphdemo.ui.employee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.apolisrises.navigationgraphdemo.R
+
+class AllEmployeesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_employees)
+    }
+}
